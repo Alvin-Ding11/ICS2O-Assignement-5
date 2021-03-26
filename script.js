afd6589f64d88990f737ecd6ae44331a592fa7e1
AlvinDing11 <alvin.ding@ocsbstudent.ca>
2021-03-26 00:33:52 +0000

    upgraded design(ish)

--- script.js
+++ script.js
@@ -7,20 +7,25 @@ let piee = 0
 document.getElementById('button').addEventListener('click', equa)
 
 function equa () {
-  diam = document.getElementById('input').value
-  diam = parseInt(diam)
+  if (diam >= 0) {
+    diam = document.getElementById('input').value
+    diam = parseInt(diam)
 
-  rad = diam / 2
-  piee = Math.PI
+    rad = diam / 2
+    piee = Math.PI
 
-  // Circumference of the Circle
-  circum = diam * piee
-  circum = circum.toFixed(2)
+    // Circumference of the Circle
+    circum = diam * piee
+    circum = circum.toFixed(2)
 
-  // Area of the Circle
-  area = piee * Math.pow(rad, 2)
-  area = area.toFixed(2)
+    // Area of the Circle
+    area = piee * Math.pow(rad, 2)
+    area = area.toFixed(2)
 
-  document.getElementById('areaans').innerHTML = '<p>' + area + '</p>'
-  document.getElementById('circumans').innerHTML = '<p>' + circum + '</p>'
+    document.getElementById('hArea').innerHTML = '<p>' + 'Area of the Circle = ' + area + '</p>'
+    document.getElementById('hCircum').innerHTML = '<p>' + 'Circumference of the Circle = ' + circum + '</p>'
+  } else {
+    document.getElementById('hArea').innerHTML = 'NaN: Error, please input a valid positive integer'
+    document.getElementById('hCircum').innerHTML = 'NaN: Error, please input a valid positive integer'
+  }
 }
