faa6c1de17cbadb57e3187557188b9775de5eb9d
AlvinDing11 <alvin.ding@ocsbstudent.ca>
2021-03-27 16:07:17 +0000

    added comments

--- script.js
+++ script.js
@@ -6,25 +6,31 @@ let piee = 0
 // Set variables, piee stands for pi as I decided to change its value inside the function
 
 document.getElementById('button').addEventListener('click', equa)
+// Event Listener that runs the equation function when the button is clicked
 
+// Function where the equation is done.
 function equa () {
   diam = document.getElementById('input').value
   diam = parseInt(diam)
 
+
+  // If statement that controls whats gets displayed. If the value of the inputed diameter is positive then it continues normaly and displays the answer. If the diameter is 0 or a negative number, then the program switchs to displaying an error message and asks the user to input a positive value.
   if (diam > 0) {
     rad = diam / 2
     piee = Math.PI
 
-    // Circumference of the Circle
+    // Equation for the circumference of the circle
     circum = diam * piee
     circum = circum.toFixed(2)
 
-    // Area of the Circle
+    // Equation for the area of the aircle
     area = piee * Math.pow(rad, 2)
     area = area.toFixed(2)
 
+    // Answer display's by accessing the headers on top of the images
     document.getElementById('harea').innerHTML = '<p>' + 'Area of the Circle = ' + area + '</p>'
     document.getElementById('hcircum').innerHTML = '<p>' + 'Circumference of the Circle = ' + circum + '</p>'
+    // Eror message display
   } else if (diam <= 0) {
     document.getElementById('harea').innerHTML = 'NaN: Error, please input a valid positive integer'
     document.getElementById('hcircum').innerHTML = 'NaN: Error, please input a valid positive integer'
